6d50895d089886a8026b10afb8d1e4bca5eeec54
Simon Gurcke <simon@gurcke.de>
2024-01-17 23:28:53 +1000

    Fix

--- src/common/packageVersions.ts
+++ src/common/packageVersions.ts
@@ -3,7 +3,7 @@ import { createRequire } from "module";
 export function getPackageVersion(name: string): string | null {
   try {
     const _require = createRequire(import.meta.url);
-    return _require(`${name}/package.json`).version || null; // eslint-disable-line @typescript-eslint/no-var-requires
+    return _require(`${name}/package.json`).version || null;
   } catch (error) {
     return null;
   }
