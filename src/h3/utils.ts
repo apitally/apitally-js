d92971627e0dcd5cf4d965f88820deb7f75e7005
Simon Gurcke <simon@gurcke.de>
2025-07-08 16:15:17 +1000

    Fix H3 plugin for 2.0.0-beta.1

--- src/h3/utils.ts
+++ src/h3/utils.ts
@@ -1,10 +1,8 @@
-import { H3, H3Route } from "h3";
+import { H3Core } from "h3";
 
 import { getPackageVersion } from "../common/packageVersions.js";
 
-export function getAppInfo(h3: H3, appVersion?: string) {
-  const routes = h3._routes ? listAllRoutes<H3Route>(h3._routes.root) : [];
-
+export function getAppInfo(h3: H3Core, appVersion?: string) {
   const versions: Array<[string, string]> = [];
   if (process.versions.node) {
     versions.push(["nodejs", process.versions.node]);
@@ -25,7 +23,7 @@ export function getAppInfo(h3: H3, appVersion?: string) {
   }
 
   return {
-    paths: routes
+    paths: h3._routes
       .map((route) => ({
         method: route.method || "",
         path: route.route || "",
@@ -39,52 +37,4 @@ export function getAppInfo(h3: H3, appVersion?: string) {
     versions: Object.fromEntries(versions),
     client: "js:h3",
   };
-}
-
-// Types from rou3
-type MethodData<T = unknown> = { data: T };
-interface Node<T = unknown> {
-  key: string;
-  static?: Record<string, Node<T>>;
-  param?: Node<T>;
-  wildcard?: Node<T>;
-  methods?: Record<string, MethodData<T>[] | undefined>;
-}
-
-function listAllRoutes<T>(node: Node<T>): T[] {
-  const routes: T[] = [];
-  _collectAllRoutes(node, routes);
-  return routes;
-}
-
-function _collectAllRoutes<T>(node: Node<T>, routes: T[] = []): void {
-  // Collect routes from current node methods
-  if (node.methods) {
-    for (const methodData of Object.values(node.methods)) {
-      if (Array.isArray(methodData)) {
-        for (const item of methodData) {
-          if (item.data) {
-            routes.push(item.data);
-          }
-        }
-      }
-    }
-  }
-
-  // Traverse static children
-  if (node.static) {
-    for (const staticChild of Object.values(node.static)) {
-      _collectAllRoutes(staticChild, routes);
-    }
-  }
-
-  // Traverse param child
-  if (node.param) {
-    _collectAllRoutes(node.param, routes);
-  }
-
-  // Traverse wildcard child
-  if (node.wildcard) {
-    _collectAllRoutes(node.wildcard, routes);
-  }
 }
